3fdc08e10b29ea36ea5d3a7f90ce26421d4df89f
Grayton Ward <graytonw@jfrog.com>
2024-01-16 03:03:19 -0500

    feat: added default fish path setup

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,8 @@ set -g __fish_home_manager_config_sourced 1
 
 set -U fish_greeting ""
 
+fish_add_path $HOME/.cargo/bin
+
 status --is-interactive; and begin
     source ~/.config/fish/alias.fish
 end
