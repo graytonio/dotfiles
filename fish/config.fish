3da0b4e0b5956ad6fd702e28c5280c8e9377e24f
Grayton Ward <graytonw@jfrog.com>
2024-01-23 23:35:33 -0500

    fix: added more paths

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,8 @@ set -g __fish_home_manager_config_sourced 1
 set -U fish_greeting ""
 
 fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.pyenv/bin
+fish_add_path $HOME/go/bin
 fish_add_path /opt/homebrew/bin
 
 source ~/.config/fish/alias.fish
