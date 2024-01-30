17832859a5bee442a26c98426a02e9f43545e91a
Grayton Ward <graytonw@jfrog.com>
2024-01-30 09:50:16 -0500

    feat: update configs

--- fish/alias.fish
+++ fish/alias.fish
@@ -7,6 +7,7 @@ alias .... 'cd ../../..'
 
 alias cat 'bat'
 alias grep 'rg'
+alias vim 'nvim'
 alias la 'eza -a --color=always --group-directories-first --icons'
 alias ll 'eza -l --color=always --group-directories-first --icons'
 alias ls 'eza -al --color=always --group-directories-first --icons'
